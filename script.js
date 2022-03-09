88218de169b3af6f808a8f5755f91123ab5d2e4b
Harshitk002 <hk7017438939@gmail.com>
2022-03-09 12:28:27 +0530

    added ML

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Front-End Web-Developer", "Competitive Programmer"],
+        strings: ["Front-End Web-Developer", "Competitive Programmer", "ML Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Front-End Web-Developer", "Competitive Programmer"],
+        strings: ["Front-End Web-Developer", "Competitive Programmer", "ML Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
